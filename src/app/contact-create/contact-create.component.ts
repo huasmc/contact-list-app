1477a52bf53f554d50e7b7f81f91ad44d33802fd
Huascar <huascarmc@hotmail.com>
2020-01-30 22:47:39 -0400

    css changes, router redirect after form submission

--- src/app/contact-create/contact-create.component.ts
+++ src/app/contact-create/contact-create.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 import { ContactService } from '../contact.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-contact-create',
@@ -13,7 +14,8 @@ export class ContactCreateComponent implements OnInit {
 
   constructor(
     private formBuilder: FormBuilder,
-    private contactService: ContactService
+    private contactService: ContactService,
+    private router: Router,
     ) {
  }
 
@@ -23,12 +25,11 @@ export class ContactCreateComponent implements OnInit {
         number: '',
         email: ''
     })
-    var contact = { "name": "Test", "number": "809-545-4067" };
-    this.contactService.addContact(contact);
-    
   }
 
-  onSubmit(contactDate) {
-    console.warn(contactDate);
+  onSubmit(contactData) {
+    console.warn(contactData);
+    this.contactService.addContact(contactData);
+    this.router.navigate(['/']);
   }
 }
