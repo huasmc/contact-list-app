34c38825f0a5dac9359d4b8c04dbf39f387a3df5
Huascar <huascarmc@hotmail.com>
2020-01-30 23:18:01 -0400

    created contact details component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { TopBarComponent } from './top-bar/top-bar.component';
 import { ContactListComponent } from './contact-list/contact-list.component';
 import { ContactCreateComponent } from './contact-create/contact-create.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { ContactDetailsComponent } from './contact-details/contact-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TopBarComponent,
     ContactListComponent,
-    ContactCreateComponent
+    ContactCreateComponent,
+    ContactDetailsComponent
   ],
   imports: [
     BrowserModule,
@@ -21,6 +23,7 @@ import { ReactiveFormsModule } from '@angular/forms';
     RouterModule.forRoot([
       { path: '', component: ContactListComponent },
       { path: 'create', component: ContactCreateComponent },
+      { path: 'contacts/:contactId', component: ContactDetailsComponent },
     ])
   ],
   providers: [],
---
+++ src/app/contact-details/contact-details.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ContactService } from '../contact.service';
+
+@Component({
+  selector: 'app-contact-details',
+  templateUrl: './contact-details.component.html',
+  styleUrls: ['./contact-details.component.css']
+})
+export class ContactDetailsComponent implements OnInit {
+
+  contact;
+  contacts;
+
+  constructor(
+    private route: ActivatedRoute,
+    private contactService: ContactService
+  ) {
+    this.contacts = this.contactService.getContacts();
+   }
+
+  ngOnInit() {
+    this.route.paramMap.subscribe(params => {
+      this.contact = this.contacts[+params.get('contactId')];
+      console.log(this.contact);
+    })
+  }
+
+}
