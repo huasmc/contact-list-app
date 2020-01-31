254ce8c85c6cb6ac6e0997c94d1985a136b09d77
Huascar <huascarmc@hotmail.com>
2020-01-30 23:56:46 -0400

    added delete button

--- src/app/contact-update/contact-update.component.ts
+++ src/app/contact-update/contact-update.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FormBuilder } from '@angular/forms';
+import { ContactService } from '../contact.service';
 
 @Component({
   selector: 'app-contact-update',
@@ -7,9 +10,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactUpdateComponent implements OnInit {
 
-  constructor() { }
+  contact;
+  updateForm;
+  contacts;
+
+  constructor(
+    private route: ActivatedRoute,
+    private formBuilder: FormBuilder,
+    private contactService: ContactService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+    this.contacts = this.contactService.getContacts();
+
+    this.route.paramMap.subscribe(params => {
+      this.contact = this.contacts[+params.get('contact')];
+    })
   }
 
 }
